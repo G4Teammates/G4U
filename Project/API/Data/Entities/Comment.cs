835fdf953ba0f883ec52b4a7714a6c88ff9ca006
HKiet-dev <huynhkiet.dev@gmail.com>
2024-08-22 22:50:54 +0700

    update entity

---
+++ Project/API/Data/Entities/Comment.cs
@@ -0,0 +1,16 @@
+﻿namespace API.Data.Entities
+{
+    public class Comment
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Content { get; set; }
+        public User User { get; set; }
+        public Guid UserId { get; set; }
+        public Product Product { get; set; }
+        public Guid ProductId { get; set; }
+        public Comment Parent { get; set; }
+        public Guid ParentId { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Project/API/Data/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿namespace API.Data.Entities
+{
+    public class Product
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; } = 0
+        public int NumberOfViews { get; set; } = 0;
+        public int NumberOfPlays { get; set; } = 0;
+        public int NumberOfLikes { get; set; } = 0;
+        public TypePlatform Platform { get; set; }
+        public StatusProduct Status { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; }
+
+        public User User { get; set; }
+        public Guid UserId { get; set; }
+    }
+}
