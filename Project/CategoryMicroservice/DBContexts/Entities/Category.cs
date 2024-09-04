2cf7a809183fcb31a1d29ffc7ade24f353a681b2
HKiet-dev <huynhkiet.dev@gmail.com>
2024-09-05 06:34:49 +0700

    Update Category.cs

--- Project/CategoryMicroservice/DBContexts/Entities/Category.cs
+++ Project/CategoryMicroservice/DBContexts/Entities/Category.cs
@@ -1,10 +1,13 @@
 ﻿using CategoryMicroservice.DBContexts.Enum;
+using System.ComponentModel.DataAnnotations;
 
 namespace CategoryMicroservice.DBContexts.Entities
 {
     public class Category
     {
         public Guid Id { get; set; } = Guid.NewGuid();
+        [Required(ErrorMessage ="Name is require")]
+        [MaxLength(100)]
         public required string Name { get; set; }
         public CategoryType Type { get; set; }
         public string? Description { get; set; }
