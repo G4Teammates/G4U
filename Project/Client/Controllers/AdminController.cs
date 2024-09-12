e28b75a227d6dbe8aeefe02a2187a7806a18454e
BeeNguyen2002 <nhan87209@gmail.com>
2024-09-12 13:17:38 +0700

    add

---
+++ Project/Client/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+
+        public IActionResult UsersManager()
+        {
+            return View();
+        }
+    }
+}
