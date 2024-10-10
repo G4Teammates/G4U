58be00c2c07ed28a55bf0c62845ab8535a321fc7
0338547322 <hungtnps28000@fpt.edu.vn>
2024-10-10 21:07:01 +0700

    gateWay

---
+++ Project/Gateway/Program.cs
@@ -0,0 +1,33 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Thêm Ocelot
+
+IConfiguration configuration = new ConfigurationBuilder().AddJsonFile("ocelot.json").Build();
+builder.Services.AddOcelot(configuration);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.UseOcelot();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
