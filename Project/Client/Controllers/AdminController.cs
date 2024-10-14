35c75b997a2b7a329a2f1a5b9f6ee0a6400aaa63
BeeNguyen2002 <nhan87209@gmail.com>
2024-10-15 02:01:42 +0700

    getall, delete product

--- Project/Client/Controllers/AdminController.cs
+++ Project/Client/Controllers/AdminController.cs
@@ -1,6 +1,9 @@
 ﻿using Client.Models;
+using Client.Models.Product_Model;
+using Client.Models.Product_Model.DTO;
 using Client.Models.UserDTO;
 using Client.Repositories.Interfaces;
+using Client.Repositories.Interfaces.ProductInterface;
 using Client.Repositories.Interfaces.User;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -8,11 +11,12 @@ using Newtonsoft.Json;
 
 namespace Client.Controllers
 {
-    public class AdminController(IUserService userService, IHelperService helperService) : Controller
+    public class AdminController(IUserService userService, IHelperService helperService, IRepoProduct productService) : Controller
     {
 
         public readonly IUserService _userService = userService;
         public readonly IHelperService _helperService = helperService;
+        public readonly IRepoProduct _productService = productService;
         public IActionResult Index()
         {
             return View();
@@ -151,10 +155,108 @@ namespace Client.Controllers
         }
 
 
-        public IActionResult ProductsManager()
+        /////////////////////////////////////////////////////
+        //                                                 //
+        //                     PRODUCT                     //
+        //                                                 //
+        /////////////////////////////////////////////////////
+        
+        // GetAll Product
+        public async Task<IActionResult> ProductsManager()
         {
+            List<ProductModel?> list = new();
+            ResponseModel? response = await _productService.GetAllProductAsync();
+
+            if (response != null && response.IsSuccess)
+            {
+
+                list = JsonConvert.DeserializeObject<List<ProductModel>>(Convert.ToString(response.Result));
+
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(list);
+        }
+
+        //Create Product
+        public async Task<IActionResult> ProductCreate()
+        {
+            // Optionally, you can fetch categories or other necessary data here
+            ViewBag.Categories = await _productService.GetCategoriesAsync(); // Fetch categories for the view
             return View();
         }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductCreate(CreateProductModel createProduct, List<IFormFile> imageFiles, IFormFile gameFile)
+        {
+            if (ModelState.IsValid)
+            {
+                // Prepare scan request for the game file
+                var scanRequest = new ScanFileRequest
+                {
+                    gameFile = gameFile // Pass the game file from the form
+                };
+
+                // Assign image files and scan request to the CreateProductModel
+                createProduct.ImageFiles = imageFiles;
+                createProduct.Request = scanRequest;
+
+                // Call the service to create the product
+                ResponseModel? response = await _productService.CreateProductAsync(createProduct);
+
+                if (response != null && response.IsSuccess)
+                {
+                    TempData["success"] = "Product created successfully";
+                    return RedirectToAction(nameof(ProductsManager)); // Redirect to the products list page
+                }
+                else
+                {
+                    TempData["error"] = response?.Message;
+                }
+            }
+
+            // If ModelState is invalid, reload categories and return the view with errors
+            ViewBag.Categories = await _productService.GetCategoriesAsync();
+            return View(createProduct);
+        }
+
+
+        //Delete Product
+        public async Task<IActionResult> ProductDelete(string id)
+        {
+            ResponseModel? response = await _productService.GetProductByIdAsync(id);
+
+            if (response != null && response.IsSuccess)
+            {
+                ProductModel? model = JsonConvert.DeserializeObject<ProductModel>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ProductDelete(ProductModel product)
+        {
+            ResponseModel? response = await _productService.DeleteProductAsync(product.Id);
+
+            if (response != null && response.IsSuccess)
+            {
+                TempData["success"] = "Coupon deleted successfully";
+                return RedirectToAction(nameof(ProductsManager));
+            }
+            else
+            {
+                TempData["error"] = response?.Message;
+            }
+            return View(product);
+        }
+
 
         public IActionResult OrdersManager()
         {
