2f0272e928a173f024db7c2e27fa89cf07e5c8a0
0338547322 <hungtnps28000@fpt.edu.vn>
2024-10-22 10:50:11 +0700

    Revert "Merge branch 'main' into Hungnekn12"
    This reverts commit 9d40c1baa5eaf356a9cb6d86359f7007c99c7ae3, reversing
    changes made to d13e4371f41911a81d54fe70d25c2176ddf0a78c.

--- Project/Client/Repositories/Services/Product/RepoProduct.cs
+++ Project/Client/Repositories/Services/Product/RepoProduct.cs
@@ -11,10 +11,11 @@ using System.Net.Http.Headers;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using Client.Repositories.Interfaces;
+using IRepoProduct = Client.Repositories.Interfaces.Product.IRepoProduct;
 
 namespace Client.Repositories.Services.Product
 {
-    public class RepoProduct : Interfaces.Product.IRepoProduct // Chỉ định không gian tên đầy đủ
+    public class RepoProduct : IRepoProduct // Chỉ định không gian tên đầy đủ
     {
         public readonly IBaseService _baseService;
         private readonly HttpClient _httpClient;
@@ -25,73 +26,70 @@ namespace Client.Repositories.Services.Product
             _httpClient = httpClient;
         }
 
-        public async Task<ResponseModel> CreateProductAsync(List<IFormFile> imageFiles, CreateProductModel Product, IFormFile gameFiles, string username)
+        public async Task<ResponseModel> CreateProductAsync(
+            string name,
+            string description,
+            decimal price,
+            float discount,
+            List<string> categories,
+            int platform,
+            int status,
+            List<IFormFile> imageFiles,
+            ScanFileRequest request,
+            string username)
         {
             // Tạo MultipartFormDataContent để gửi dữ liệu
             var formData = new MultipartFormDataContent();
 
-            // Thêm các thông tin sản phẩm vào formData
-            formData.Add(new StringContent(Product.Name), "name");
-            formData.Add(new StringContent(Product.Description ?? string.Empty), "description");
-            formData.Add(new StringContent(Product.Price.ToString()), "price");
-            formData.Add(new StringContent(Product.Discount.ToString()), "discount");
-            formData.Add(new StringContent(((int)Product.Platform).ToString()), "platform");
-            formData.Add(new StringContent(((int)Product.Status).ToString()), "status");
+            formData.Add(new StringContent(name), "name");
+            formData.Add(new StringContent(description ?? string.Empty), "description");
+            formData.Add(new StringContent(price.ToString()), "price");
+            formData.Add(new StringContent(discount.ToString()), "discount");
+            formData.Add(new StringContent(platform.ToString()), "platform");
+            formData.Add(new StringContent(status.ToString()), "status");
             formData.Add(new StringContent(username), "username");
 
             // Thêm các danh mục vào formData
-            if (Product.Categories != null)
+            foreach (var category in categories)
             {
-                foreach (var category in Product.Categories)
-                {
-                    formData.Add(new StringContent(category.CategoryName.ToString()), "categories"); // Giả sử mỗi danh mục có thuộc tính Id
-                }
+                formData.Add(new StringContent(category), "categories");
             }
 
-            // Thêm tệp hình ảnh vào formData
+            // Thêm các tệp hình ảnh vào formData
             foreach (var file in imageFiles)
             {
                 var fileContent = new StreamContent(file.OpenReadStream());
-                fileContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
-                formData.Add(fileContent, "imageFiles", file.FileName);
+                fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                {
+                    Name = "imageFiles",
+                    FileName = file.FileName
+                };
+                formData.Add(fileContent);
             }
 
             // Thêm tệp game vào formData
-            if (gameFiles != null)
+            if (request?.gameFile != null)
             {
-                var gameFileContent = new StreamContent(gameFiles.OpenReadStream());
-                gameFileContent.Headers.ContentType = new MediaTypeHeaderValue(gameFiles.ContentType);
-                formData.Add(gameFileContent, "gameFiles", gameFiles.FileName);
+                var gameFileContent = new StreamContent(request.gameFile.OpenReadStream());
+                gameFileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                {
+                    Name = "gameFile",
+                    FileName = request.gameFile.FileName
+                };
+                formData.Add(gameFileContent);
             }
 
             // Gửi yêu cầu POST thông qua base service
             var response = await _baseService.SendAsync(new RequestModel()
             {
                 ApiType = StaticTypeApi.ApiType.POST,
-                Url = StaticTypeApi.APIGateWay + "/Product",
-                Data = formData
+                Data = formData,
+                Url = StaticTypeApi.APIGateWay + "/Product"
             });
 
             return response;
         }
 
-
-        public Task<ResponseModel> DeleteProductAsync(string Id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<ResponseModel> DeleteProductAsysnc(string Id)
-        {
-            return await _baseService.SendAsync(new RequestModel()
-            {
-                ApiType = StaticTypeApi.ApiType.DELETE,
-                Url = StaticTypeApi.APIGateWay + "/Product/" + Id
-            });
-        }
-
-
-       
         public async Task<ResponseModel?> GetAllProductAsync()
         {
             return await _baseService.SendAsync(new RequestModel()
@@ -110,19 +108,69 @@ namespace Client.Repositories.Services.Product
             });
         }
 
-            public async Task<ResponseModel?> SearchProductAsync(string? query) // Sửa tên phương thức
+        public async Task<ResponseModel> UpdateProductAsync(string id,
+                                                            string name,
+                                                            string description,
+                                                            decimal price,
+                                                            int sold,
+                                                            int numOfView,
+                                                            int numOfLike,
+                                                            float discount,
+                                                            List<string> categories,
+                                                            int platform,
+                                                            int status,
+                                                            DateTime createAt,
+                                                            List<IFormFile> imageFiles,
+                                                            ScanFileRequest request,
+                                                            string username)
+        {
+            var formData = new MultipartFormDataContent();
+
+            // Thêm các tham số vào formData
+            formData.Add(new StringContent(id), "id");
+            formData.Add(new StringContent(name), "name");
+            formData.Add(new StringContent(description), "description");
+            formData.Add(new StringContent(price.ToString()), "price");
+            formData.Add(new StringContent(sold.ToString()), "sold");
+            formData.Add(new StringContent(numOfView.ToString()), "numOfView");
+            formData.Add(new StringContent(numOfLike.ToString()), "numOfLike");
+            formData.Add(new StringContent(discount.ToString()), "discount");
+            formData.Add(new StringContent(platform.ToString()), "platform");
+            formData.Add(new StringContent(status.ToString()), "status");
+            formData.Add(new StringContent(createAt.ToString("o")), "createAt");
+            formData.Add(new StringContent(username), "username");
+
+            // Thêm các danh mục vào formData
+            foreach (var category in categories)
             {
-            // Thực hiện gửi yêu cầu tìm kiếm qua API Gateway
-                return await _baseService.SendAsync(new RequestModel
-                {
-                    ApiType = StaticTypeApi.ApiType.GET,
-                    Url = StaticTypeApi.APIGateWay + $"/Product/search" // Encode URL để tránh lỗi
-                });
+                formData.Add(new StringContent(category), "categories");
             }
 
-        public Task<ResponseModel> UpdateProductAsync(List<IFormFile> imageFiles, UpdateProductModel product, ScanFileRequest request)
-        {
-            throw new NotImplementedException();
+            // Thêm tệp hình ảnh vào formData
+            foreach (var file in imageFiles)
+            {
+                var fileContent = new StreamContent(file.OpenReadStream());
+                fileContent.Headers.ContentType = new MediaTypeHeaderValue(file.ContentType);
+                formData.Add(fileContent, "imageFiles", file.FileName);
+            }
+
+            // Thêm tệp trò chơi từ ScanFileRequest
+            if (request != null && request.gameFile != null)
+            {
+                var gameFileContent = new StreamContent(request.gameFile.OpenReadStream());
+                gameFileContent.Headers.ContentType = new MediaTypeHeaderValue(request.gameFile.ContentType);
+                formData.Add(gameFileContent, "request.gameFile", request.gameFile.FileName);
+            }
+
+            // Gửi yêu cầu PUT thông qua base service
+            var response = await _baseService.SendAsync(new RequestModel()
+            {
+                ApiType = StaticTypeApi.ApiType.PUT,
+                Url = StaticTypeApi.APIGateWay + "/Product",
+                Data = formData
+            });
+
+            return response;
         }
     }
 }
