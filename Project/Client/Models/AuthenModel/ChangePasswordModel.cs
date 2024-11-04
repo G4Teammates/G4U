50b4260afa0f3da7da400fef117e4358a9d0c850
HKiet-dev <huynhkiet.dev@gmail.com>
2024-11-04 20:37:48 +0700

    update noti error

---
+++ Project/Client/Models/AuthenModel/ChangePasswordModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Client.Models.AuthenModel
+{
+    public class ChangePasswordModel
+    {
+        [Required(ErrorMessage ="Vui lòng nhập mật khẩu trước đó")]
+        [StringLength(64, ErrorMessage = "Mật khẩu cũ phải ít nhất {2} và tối đa {1} kí tự", MinimumLength = 6)]
+        public string? OldPassword { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập mật khẩu mới đó")]
+        [StringLength(64, ErrorMessage = "Mật khẩu mới phải ít nhất {2} và tối đa {1} kí tự.", MinimumLength = 6)]
+        public string? NewPassword { get; set; }
+        [Required(ErrorMessage = "Vui lòng nhập xác nhận mật khẩu đó")]
+        [Compare(nameof(NewPassword),ErrorMessage = "Xác nhận mật khẩu không trùng khớp")]
+        [StringLength(64, ErrorMessage = "Xác nhận mật khẩu phải ít nhất {2} và tối đa {1} kí tự.", MinimumLength = 6)]
+        public string? ConfirmPassword { get; set; }
+    }
+}
