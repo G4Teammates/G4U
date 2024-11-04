aca7a03a2095db7049e288b01837954bca6629e5
HKiet-dev <huynhkiet.dev@gmail.com>
2024-11-04 23:43:33 +0700

    check error

--- Project/UserMicroservice/Models/AuthModel/LoginGoogleRequestModel.cs
+++ Project/UserMicroservice/Models/AuthModel/LoginGoogleRequestModel.cs
@@ -1,12 +1,21 @@
-﻿using UserMicroservice.DBContexts.Enum;
+﻿using System.ComponentModel.DataAnnotations;
+using UserMicroservice.DBContexts.Enum;
 
 namespace UserMicroservice.Models.AuthModel
 {
     public class LoginGoogleRequestModel
     {
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(320, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         public string? Username { get; set; }
+        [StringLength(256, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
         public string? DisplayName { get; set; }
+        [Required(ErrorMessage = "The {0} field is required.")]
+        [StringLength(320, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
+        [EmailAddress(ErrorMessage = "The {0} field is not a valid e-mail address.")]
         public string? Email { get; set; }
+        [Url(ErrorMessage = "The {0} field is not a valid URL.")]
+        [MaxLength(2048)]
         public string? Picture { get; set; }
         public EmailStatus EmailConfirmation { get; set; }
     }
