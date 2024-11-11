75f3cdb372914167c63e96e21d2eb1edeb740be5
TuanMinh1011 <129381372+TuanMinh1011@users.noreply.github.com>
2024-11-12 01:14:55 +0700

    update update edit product ngon
    chua test ky

--- Project/Client/Models/ProductDTO/UpdateProductModel.cs
+++ Project/Client/Models/ProductDTO/UpdateProductModel.cs
@@ -7,20 +7,20 @@ namespace Client.Models.ProductDTO
     public class UpdateProductModel
     {
 
-            public string Id { get; set; }
-            public string Name { get; set; }
-            public string? Description { get; set; }
-            public decimal Price { get; set; }
-            public int Sold { get; set; }
-            public InteractionModel? Interactions { get; set; } = new InteractionModel();
-            public float Discount { get; set; }
-            public List<LinkModel> Links { get; set; }
-            public List<string> Categories { get; set; } = new List<string>();
-            public PlatformType Platform { get; set; }
-            public ProductStatus Status { get; set; }
-            public DateTime CreatedAt { get; set; }
-            public List<IFormFile>? ImageFiles { get; set; } // Để xử lý file upload
-            public IFormFile? gameFile { get; set; } // Giả sử bạn có model này cho game file 
-            public string UserName { get; set; }
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Sold { get; set; }
+        public InteractionModel? Interactions { get; set; } = new InteractionModel();
+        public float Discount { get; set; }
+        public List<LinkModel> Links { get; set; }
+        public List<string> Categories { get; set; } = new List<string>();
+        public PlatformType Platform { get; set; }
+        public ProductStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public List<IFormFile>? ImageFiles { get; set; } = new List<IFormFile>(); // Để xử lý file upload
+        public IFormFile? gameFile { get; set; } // Giả sử bạn có model này cho game file 
+        public string UserName { get; set; }
     }
 }
