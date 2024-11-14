6cc10dfc60aee93dfca0d990729e4b70eb40ded2
0338547322 <hungtnps28000@fpt.edu.vn>
2024-11-14 07:43:22 +0700

    ok

--- Project/UserMicroservice/Controllers/UserController.cs
+++ Project/UserMicroservice/Controllers/UserController.cs
@@ -23,7 +23,7 @@ namespace UserMicroService.Controllers
         private readonly IUserService _userService = userService;
 
 
-        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme,Roles = "User")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Admin")]
         [HttpGet]
         public async Task<ActionResult> GetAll(int? page, int pageSize)
         {
