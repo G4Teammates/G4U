ba29158334ec006191fbcd174be27760401d6d4c
TanSieuDepTrai <votan070520@gmail.com>
2024-11-21 05:01:52 +0700

    StastisticalUser

---
+++ Project/Client/Models/Statistical/ProductGroupByUserData.cs
@@ -0,0 +1,9 @@
+﻿namespace Client.Models.Statistical
+{
+    public class ProductGroupByUserData
+    {
+        public int Views { get; set; }
+        public int Products { get; set; }
+        public int Solds { get; set; }
+    }
+}
